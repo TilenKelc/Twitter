25dc98d314ef4d2c448eda53c5411fcf106ffa9c
Tilen <tilen.kelc@github.com>
2019-10-05 16:26:03 +0200

    Revert "Updated database"
    This reverts commit 33183032ea602111fb30c3a97f174adadc1497b3.

--- sql-database/twitter-data.sql
+++ sql-database/twitter-data.sql
@@ -35,6 +35,14 @@ CREATE TABLE `friends` (
   `friend_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Odloži podatke za tabelo `friends`
+--
+
+INSERT INTO `friends` (`id`, `user_id`, `state`, `friend_id`) VALUES
+(11, 8, 'Following', 3);
+
+-- --------------------------------------------------------
 
 --
 -- Struktura tabele `replies`
@@ -48,6 +56,18 @@ CREATE TABLE `replies` (
   `tweet_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Odloži podatke za tabelo `replies`
+--
+
+INSERT INTO `replies` (`id`, `user_id`, `reply`, `date`, `tweet_id`) VALUES
+(1, 8, 'asd', '2019-09-17 20:07:46', 1),
+(2, 8, 'asd', '2019-09-17 20:07:49', 1),
+(3, 8, 'Not much', '2019-09-26 10:48:24', 1),
+(4, 8, 'N', '2019-09-26 12:30:33', 1),
+(5, 8, 'What event', '2019-09-26 13:00:43', 2);
+
+-- --------------------------------------------------------
 
 --
 -- Struktura tabele `tweets`
@@ -64,6 +84,24 @@ CREATE TABLE `tweets` (
   `reported` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Odloži podatke za tabelo `tweets`
+--
+
+INSERT INTO `tweets` (`id`, `user_id`, `picture`, `text`, `time`, `likes`, `like_id`, `reported`) VALUES
+(8, 8, NULL, 'sad', '2019-09-29 13:15:30', 0, NULL, 0),
+(9, 8, NULL, 'asd', '2019-09-29 13:15:31', 0, NULL, 0),
+(10, 8, NULL, 'asd', '2019-09-29 13:15:33', 0, NULL, 0),
+(6, 8, NULL, 'asdasd', '2019-09-29 10:59:50', 0, NULL, 0),
+(7, 8, NULL, 'b', '2019-09-29 12:09:14', 0, NULL, 0),
+(11, 8, NULL, 'asd', '2019-09-29 13:15:34', 0, NULL, 0),
+(12, 8, NULL, 'asd', '2019-09-29 13:15:39', 0, NULL, 0),
+(13, 8, NULL, 'asdasda', '2019-09-29 13:15:43', 0, NULL, 0),
+(14, 8, NULL, 'qweqwe', '2019-09-29 13:18:38', 0, NULL, 0),
+(15, 8, NULL, 'asdasd', '2019-09-29 13:18:40', 0, NULL, 0),
+(16, 10, NULL, 'nc', '2019-10-01 15:38:42', 0, NULL, 0);
+
+-- --------------------------------------------------------
 
 --
 -- Struktura tabele `types`
@@ -74,6 +112,15 @@ CREATE TABLE `types` (
   `user_type` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Odloži podatke za tabelo `types`
+--
+
+INSERT INTO `types` (`id`, `user_type`) VALUES
+(1, 'Administrator'),
+(2, 'User');
+
+-- --------------------------------------------------------
 
 --
 -- Struktura tabele `users`
@@ -92,6 +139,15 @@ CREATE TABLE `users` (
   `type_id` bigint(20) UNSIGNED NOT NULL DEFAULT '2'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Odloži podatke za tabelo `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `joined`, `bio`, `location`, `born`, `avatar`, `password`, `email`, `type_id`) VALUES
+(4, 'mts', '2019-09-12', NULL, NULL, NULL, NULL, '$2y$10$T76hOevhSyxy7vj22ANx0Op9NIC27Qp88DRo.m1wtDakHP4ODgy4e', 'tilen@gmail.com', 2),
+(3, '123', '2019-09-10', NULL, NULL, NULL, NULL, '$2y$10$zYk4eYILDdGHpgeLojQZmu3bLWIRuLY5YCGZ506MF3G5PTP4QPyta', 'tilenkelc@gmail.com', 2),
+(8, 'Tilen Kelc', '2019-09-15', '', '', '0000-00-00', NULL, 'google', 'tilen.kelc@gmail.com', 1),
+(10, '123123', '2019-09-27', '', '', '0000-00-00', 'Zajeta slika.PNG', '$2y$10$/GcV0JCLq9aoEJK5pw1Us.CJ0DQ6r13ko7dMthf.koKSaUNmCwjG2', 'kelc@gmail.com', 2);
 
 --
 -- Struktura tabele `likeIDs`
